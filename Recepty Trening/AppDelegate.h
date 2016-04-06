3ccc91532adb02428987b5ce8e3e6ce0b6a758cc
Rafal Kampa <rafalkampa@MacBook-Air-Rafal.local>
2016-04-06 16:01:14 +0200

    Sprawdzanie

---
+++ Recepty Trening/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recepty Trening
+//
+//  Created by Rafal Kampa on 06.04.2016.
+//  Copyright © 2016 Rafal Kampa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
